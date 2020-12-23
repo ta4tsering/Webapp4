d91ae93511ea7f6bdaec21a4ebdf84b4cc89253a
Tashi Tsering <ta3tsering@gmail.com>
2020-12-23 13:42:32 +0530

    added the utils.py

--- webapp/routes.py
+++ webapp/routes.py
@@ -26,7 +26,7 @@ def OCR():
             elif request.method == 'POST':
                 if request.files:
                     work_file = request.files["work_file"]
-                    work_file.save(os.path.join(app.config["FILES_UPLOAD"], work_file.filename))
-                    return f'OCR is running on engine {OCR_engine}'
-                utility()
+                    work_file.save(os.path.join(app.config["FILES_UPLOAD"], 'file.txt'))
+                    work_list = utility()
+                    return f'OCR is running on engine {OCR_engine}, on work id {work_list}   '
     return render_template('OCR.html', title='OCR', form=form)
--- webapp/utils.py
+++ webapp/utils.py
@@ -5,19 +5,10 @@ from webapp import app
 
 
 def utility():
-    work_path = Path(os.path.join(app.config["FILES_UPLOAD"]))
+    work_path = Path("/Users/tashitsering/Desktop/W4OCR/webapp/static/files/file.txt")
     work_text = work_path.read_text(encoding="utf-8")
-    result = []
-    pg_text = ""
-    work_keys = re.split(f"\n", work_text)
-    for i, work_key in enumerate(work_keys[1:],1):
-        if i % 2 == 0:
-            pg_text += work_key
-        else:
-            pg_text += work_key
-            result.append(pg_text)
-            pg_text = ""
-    return print(result)
+    result = work_text.splitlines()
+    return result
         
         
     
